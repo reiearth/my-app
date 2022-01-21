2581444d553485e405c713a6a7343b142cca5521
reiearth <reionlinebiz@gmail.com>
2022-01-22 04:54:18 +0800

    complete the card component and make the result section responsive

--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
-import { ThumbUpIcon } from '@heroicons/react/solid';
+import { ThumbUpIcon } from '@heroicons/react/outline';
 
 export default function Card({result}) {
     const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-  <div>
+  <div className='p-3 hover:text-white cursor-pointer active:text-red-400 xl:hover:scale-105 transition-transform duration-200'>
         <Image alt="movie thumbnails" layout="responsive" src={BASE_URL + result.backdrop_path} height={100} width={200}/>
-        <div className=''>
-            <p className='truncate'>{result.overview}</p>
-            <h2>{result.title || result.name}</h2>
-            <p>{result.release_date || result.first_air_date}</p>
-            <ThumbUpIcon className='h-5'/>
+        <div className='p-2'>
+            <p className='truncate text-lg'>{result.overview}</p>
+            <h2 className='text-lg font-bold'>{result.title || result.name}</h2>
+            <p className='flex items-center'>{result.release_date || result.first_air_date}
+            <ThumbUpIcon className='h-5 ml-3 mr-1'/>
             {result.vote_count}
+            </p>
         </div>
   </div>
   )
