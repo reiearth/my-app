96e0f0218d2148872fba438dc515f2e06f1e344b
reiearth <reionlinebiz@gmail.com>
2022-01-22 00:25:52 +0800

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+// import { ChakraProvider } from '@chakra-ui/react'
+
+function MyApp({ Component, pageProps }) {
+  return (
+ 
+        <Component {...pageProps} />
+    
+   
+  )
+}
+
+export default MyApp
