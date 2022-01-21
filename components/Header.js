015ef0e569c6585290d2c9fcfd00c819b1434dab
reiearth <reionlinebiz@gmail.com>
2022-01-22 02:57:54 +0800

    create the Navbar components and request.js

--- components/Header.js
+++ components/Header.js
@@ -20,6 +20,7 @@ export default function Header() {
                  <Image src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg" 
                  width={100} 
                  height={100}
+                 alt="IMDB logo"
                  className="cursor-pointer active:brightness-110"
                  />
 
